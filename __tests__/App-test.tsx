c22770f82d784111cb06cd6ff1a777c4139561c1
ckechad <ckechad@icloud.com>
2021-04-10 19:06:36 +0200

    test(app):instal @testing library react native and test it

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -6,9 +6,9 @@ import 'react-native';
 import React from 'react';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import {render} from '@testing-library/react-native';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const rendered = render(<App />);
+  rendered.getAllByText('Ice-Cream');
 });
